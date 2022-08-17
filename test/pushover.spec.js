8eada725afb3a541bd0a848b22132b2ce52b8b98
Kevin-Deason <kevin@deason.co.za>
2022-08-17 23:15:11 +0200

    chore: Update tests to use Jest

--- test/pushover.spec.js
+++ test/pushover.spec.js
@@ -4,7 +4,7 @@ const pushover = require("../pushover.js");
 helper.init(require.resolve('node-red'));
 
 describe('pushover', function () {
-    before(function (done) {
+    beforeAll(function (done) {
         helper.startServer(done);
     });
 
@@ -12,7 +12,7 @@ describe('pushover', function () {
         helper.unload();
     });
 
-    after(function (done) {
+    afterAll (function (done) {
         helper.stopServer(done);
     });
 
@@ -21,7 +21,7 @@ describe('pushover', function () {
         helper.load(pushover, flow, function () {
             const n1 = helper.getNode("n1");
             try {
-                n1.should.have.property('name', 'ntc-node-red-pushover-keys');
+                expect(n1.name).toMatch("ntc-node-red-pushover-keys");
                 done();
             } catch (err) {
                 done(err);
@@ -34,7 +34,7 @@ describe('pushover', function () {
         helper.load(pushover, flow, function () {
             const n2 = helper.getNode("n2");
             try {
-                n2.should.have.property('name', 'ntc-node-red-pushover-notifications');
+                expect(n2.name).toMatch("ntc-node-red-pushover-notifications");
                 done();
             } catch (err) {
                 done(err);
@@ -47,7 +47,7 @@ describe('pushover', function () {
         helper.load(pushover, flow, function () {
             const n3 = helper.getNode("n3");
             try {
-                n3.should.have.property('name', 'ntc-node-red-pushover-glances');
+                expect(n3.name).toMatch("ntc-node-red-pushover-glances");
                 done();
             } catch (err) {
                 done(err);
