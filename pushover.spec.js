6e3005262586ae58f723c0233dcfe6601822e2e0
Kevin-Deason <kevin@deason.co.za>
2022-09-08 23:06:58 +0200

    chore: improve test coverage

--- pushover.spec.js
+++ pushover.spec.js
@@ -1,7 +1,11 @@
 const helper = require("node-red-node-test-helper");
 const pushover = require("./pushover.js");
+const {v4: uuidv4} = require("uuid");
+const axios = require("axios");
 
 helper.init(require.resolve('node-red'));
+
+jest.mock('axios');
 
 describe('pushover', function () {
     beforeAll(function (done) {
@@ -12,7 +16,7 @@ describe('pushover', function () {
         helper.unload();
     });
 
-    afterAll (function (done) {
+    afterAll(function (done) {
         helper.stopServer(done);
     });
 
@@ -30,15 +34,83 @@ describe('pushover', function () {
     });
 
     it('should load ntc-node-red-pushover-notifications', function (done) {
-        const flow = [{id: "n2", type: "ntc-node-red-pushover-notifications", name: "ntc-node-red-pushover-notifications"}];
+        const flow = [{
+            id: "notification",
+            type: "ntc-node-red-pushover-notifications",
+            name: "ntc-node-red-pushover-notifications"
+        }];
         helper.load(pushover, flow, function () {
-            const n2 = helper.getNode("n2");
-            try {
-                expect(n2.name).toMatch("ntc-node-red-pushover-notifications");
-                done();
-            } catch (err) {
-                done(err);
+            const notification = helper.getNode("notification");
+
+            notification.on("input", (msg) => {
+                try {
+                    expect(msg.payload).toMatch("Test Message")
+                    done();
+                } catch (e) {
+                    done(e);
+                }
+            });
+
+            notification.receive({payload: "Test Message"});
+
+        });
+    });
+
+    it('should send a simple notification', function (done) {
+
+        const requestId = uuidv4();
+        const axiosResponse = {
+            data: {
+                "status": 1,
+                "request": requestId
             }
+        };
+
+        const libResponse = {
+            payload: {
+                "status": 1,
+                "request": requestId,
+                "receipt": null
+            }
+        };
+
+        axios.post.mockResolvedValue(axiosResponse);
+
+        const credentials = {
+            nc: {
+                userKey: "userKey",
+                token: "token"
+            }
+        }
+
+        const flow = [
+            {
+                id: "n1", type: "ntc-node-red-pushover-notifications", name: "notification", wires: [["n2"]], keys: "nc"
+            },
+            {
+                'id': 'nc',
+                'type': 'ntc-node-red-pushover-keys',
+                'displayName': 'Config'
+            },
+            {id: "n2", type: "helper"}
+        ];
+
+        helper.load(pushover, flow, credentials, function () {
+            const n2 = helper.getNode("n2");
+            const n1 = helper.getNode("n1");
+
+            n2.on("input", function (msg) {
+                try {
+                    expect(msg).toMatchObject(libResponse);
+                    done();
+                } catch (err) {
+                    done(err);
+                }
+            });
+
+            n1.receive({
+                payload: "This is a test message"
+            });
         });
     });
 
